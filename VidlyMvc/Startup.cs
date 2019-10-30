03575c71a6d72ed1e1ecf99fb96ce9387da4dfce
Tiago Pernes <pernes.9@emea.teleperformance.com>
2019-10-30 16:33:51 +0000

    Project Started

---
+++ VidlyMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VidlyMvc.Startup))]
+namespace VidlyMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
